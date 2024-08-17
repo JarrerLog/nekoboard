fe4763c6027d28fc2cb17c8ba44d7c307c05658a
nekoboard <nekoboard>
2024-08-16 22:08:04 -0700

    nekoinstall added and translation

--- app/Console/Commands/NekoInstall.php
+++ app/Console/Commands/NekoInstall.php
@@ -8,21 +8,21 @@ use App\Models\User;
 use App\Utils\Helper;
 use Illuminate\Support\Facades\DB;
 
-class V2boardInstall extends Command
+class NekoInstall extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'v2board:install';
+    protected $signature = 'neko:install';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'v2board 安装';
+    protected $description = 'NekoInstall';
 
     /**
      * Create a new command instance.
@@ -42,66 +42,76 @@ class V2boardInstall extends Command
     public function handle()
     {
         try {
-            $this->info("__     ______  ____                      _  ");
-            $this->info("\ \   / /___ \| __ )  ___   __ _ _ __ __| | ");
-            $this->info(" \ \ / /  __) |  _ \ / _ \ / _` | '__/ _` | ");
-            $this->info("  \ V /  / __/| |_) | (_) | (_| | | | (_| | ");
-            $this->info("   \_/  |_____|____/ \___/ \__,_|_|  \__,_| ");
+            $this->info("
+
+                    _   _      _         _                         _ 
+                    | \ | | ___| | _____ | |__   ___   __ _ _ __ __| |
+                    |  \| |/ _ \ |/ / _ \| '_ \ / _ \ / _` | '__/ _` |
+                    | |\  |  __/   < (_) | |_) | (_) | (_| | | | (_| |
+                    |_| \_|\___|_|\_\___/|_.__/ \___/ \__,_|_|  \__,_|
+
+                    ");
             if (\File::exists(base_path() . '/.env')) {
-                $securePath = config('v2board.secure_path', config('v2board.frontend_admin_path', hash('crc32b', config('app.key'))));
-                $this->info("访问 http(s)://你的站点/{$securePath} 进入管理面板，你可以在用户中心修改你的密码。");
-                abort(500, '如需重新安装请删除目录下.env文件');
+                $securePath = config('v2board.secure_path', config('v2board.frontend_admin_path', "neko"));
+                $this->info("https://<ip/domain>/{$securePath}");
+                abort(500, 'File .env đã tồn tại');
             }
 
             if (!copy(base_path() . '/.env.example', base_path() . '/.env')) {
-                abort(500, '复制环境文件失败，请检查目录权限');
+                abort(500, 'Gói cài đặt đã bị lỗi vui lòng tải lại');
             }
             $this->saveToEnv([
                 'APP_KEY' => 'base64:' . base64_encode(Encrypter::generateKey('AES-256-CBC')),
-                'DB_HOST' => $this->ask('请输入数据库地址（默认:localhost）', 'localhost'),
-                'DB_DATABASE' => $this->ask('请输入数据库名'),
-                'DB_USERNAME' => $this->ask('请输入数据库用户名'),
-                'DB_PASSWORD' => $this->ask('请输入数据库密码')
+                'DB_HOST' => $this->ask('Địa chỉ database (mặc định: localhost)', 'localhost'),
+                'DB_DATABASE' => $this->ask('Tên database '),
+                'DB_USERNAME' => $this->ask('Tên đăng nhập database'),
+                'DB_PASSWORD' => $this->ask('Mật khẩu database')
             ]);
             \Artisan::call('config:clear');
             \Artisan::call('config:cache');
             try {
                 DB::connection()->getPdo();
             } catch (\Exception $e) {
-                abort(500, '数据库连接失败');
+                abort(500, 'Lỗi kết nối tới database vui lòng kiểm tra database');
             }
             $file = \File::get(base_path() . '/database/install.sql');
             if (!$file) {
-                abort(500, '数据库文件不存在');
+                abort(500, 'File cài đặt bị lỗi vui lòng tải lại');
             }
             $sql = str_replace("\n", "", $file);
             $sql = preg_split("/;/", $sql);
             if (!is_array($sql)) {
-                abort(500, '数据库文件格式有误');
+                abort(500, 'File cài đặt bị lỗi vui lòng tải lại');
             }
-            $this->info('正在导入数据库请稍等...');
+            $this->info('Đang nhập database...');
             foreach ($sql as $item) {
                 try {
                     DB::select(DB::raw($item));
                 } catch (\Exception $e) {
                 }
             }
-            $this->info('数据库导入完成');
+            $this->info('Tạo thông tin admin:');
             $email = '';
+            $password = '';
             while (!$email) {
-                $email = $this->ask('请输入管理员邮箱?');
+                $email = $this->ask('Email admin?');
             }
-            $password = Helper::guid(false);
+
+            while (!$password) {
+                $password = $this->ask("Mật khẩu admin?");
+            }
+
+            // $password = Helper::guid(false);
             if (!$this->registerAdmin($email, $password)) {
-                abort(500, '管理员账号注册失败，请重试');
+                abort(500, 'Tạo user không thành công');
             }
 
-            $this->info('一切就绪');
-            $this->info("管理员邮箱：{$email}");
-            $this->info("管理员密码：{$password}");
+            $this->info('Thông tin:');
+            $this->info("Email admin: {$email}");
+            $this->info("Mật khẩu admin: {$password}");
 
-            $defaultSecurePath = hash('crc32b', config('app.key'));
-            $this->info("访问 http(s)://你的站点/{$defaultSecurePath} 进入管理面板，你可以在用户中心修改你的密码。");
+            $defaultSecurePath = "neko";
+            $this->info("truy cập http(s)://<ip/domain>/{$defaultSecurePath}");
         } catch (\Exception $e) {
             $this->error($e->getMessage());
         }
@@ -111,9 +121,9 @@ class V2boardInstall extends Command
     {
         $user = new User();
         $user->email = $email;
-        if (strlen($password) < 8) {
-            abort(500, '管理员密码长度最小为8位字符');
-        }
+        // if (strlen($password) < 8) {
+        //     abort(500, '管理员密码长度最小为8位字符');
+        // }
         $user->password = password_hash($password, PASSWORD_DEFAULT);
         $user->uuid = Helper::guid(true);
         $user->token = Helper::guid();
@@ -125,7 +135,7 @@ class V2boardInstall extends Command
     {
         function set_env_var($key, $value)
         {
-            if (! is_bool(strpos($value, ' '))) {
+            if (!is_bool(strpos($value, ' '))) {
                 $value = '"' . $value . '"';
             }
             $key = strtoupper($key);
@@ -147,7 +157,7 @@ class V2boardInstall extends Command
             fwrite($file, $contents);
             return fclose($file);
         }
-        foreach($data as $key => $value) {
+        foreach ($data as $key => $value) {
             set_env_var($key, $value);
         }
         return true;
